4b467dd34933a89abc97a77a1c226712d991ef76
Karen Calpo <minnkc@gmail.com>
2016-09-29 23:57:55 -0700

    Equal sides

--- equal_sides_array.js
+++ equal_sides_array.js
@@ -1,3 +1,10 @@
+/*
+You are going to be given an array of integers. Your job is to 
+take that array and find an index N where the sum of the integers 
+to the left of N is equal to the sum of the integers to the right 
+of N. If there is no index that would make this happen, return -1.
+*/
+
 var arr1 = [1,2,3,4,3,2,1];
 var arr2 = [1,100,50,-51,1,1];
 var arr3 = [1,2,3,4,5,6];
